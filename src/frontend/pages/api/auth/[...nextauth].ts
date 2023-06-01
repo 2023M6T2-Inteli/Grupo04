a7d1256eb12c99f0d63f26958355db6fa37ea9e5
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2023-06-01 02:42:59 -0300

    fix(frontend):svg imports & new auth

---
+++ src/frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import NextAuth from "next-auth/src";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+interface Credentials {
+    email: string;
+    password: string;
+}
+
+export default NextAuth({
+    session: {
+        strategy: "jwt",
+    },
+    providers: [
+        CredentialsProvider({
+            type: "credentials",
+            credentials: {
+                email: {label: "Email", type: "text", placeholder: "email"},
+                password: {label: "Password", type: "password", placeholder: "password"},
+            },
+            authorize: async (credentials, req) => {
+                const {email, password} = credentials as Credentials;
+
+                const response = await fetch("http://localhost:3000/api/auth/login", {
+                    method: "POST",
+                    body: JSON.stringify(credentials),
+                    headers: {"Content-Type": "application/json"}
+                });
+
+                if (response.status === 200) {
+                    const jsonResponse = await response.json();
+                    return jsonResponse;
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
+    callbacks: {
+        jwt({token, user, account, profile, isNewUser}) {
+            if (user) {
+                // @ts-ignore
+                token.accessToken = user.token;
+            }
+            return token;
+        },
+        session({session, token}) {
+            // @ts-ignore
+            session.accessToken = token.accessToken;
+            return session;
+        }
+    }
+});
